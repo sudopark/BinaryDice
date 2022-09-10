573aa0d3b1291667d2ed8a65ab6448bdc62e28d5
sudo.park <sudo.park@kakao.com>
2022-09-10 15:14:07 +0900

    ThirdParty.framwork 추가

---
+++ Supports/ThirdParty/ThirdParty/ThirdParty.h
@@ -0,0 +1,18 @@
+//
+//  ThirdParty.h
+//  ThirdParty
+//
+//  Created by sudo.park on 2022/09/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ThirdParty.
+FOUNDATION_EXPORT double ThirdPartyVersionNumber;
+
+//! Project version string for ThirdParty.
+FOUNDATION_EXPORT const unsigned char ThirdPartyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThirdParty/PublicHeader.h>
+
+
