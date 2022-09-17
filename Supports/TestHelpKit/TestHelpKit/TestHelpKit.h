f1f5d061ce69608fc7d66b90cda4dcf0e9051448
sudo.park <sudo.park@kakao.com>
2022-09-17 14:09:44 +0900

    [#8] TestHelpKit.framework 추가

---
+++ Supports/TestHelpKit/TestHelpKit/TestHelpKit.h
@@ -0,0 +1,18 @@
+//
+//  TestHelpKit.h
+//  TestHelpKit
+//
+//  Created by sudo.park on 2022/09/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestHelpKit.
+FOUNDATION_EXPORT double TestHelpKitVersionNumber;
+
+//! Project version string for TestHelpKit.
+FOUNDATION_EXPORT const unsigned char TestHelpKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestHelpKit/PublicHeader.h>
+
+
