8b4c8ace2209dfc466217b1fdf838d0b1a35938d
sudo.park <sudo.park@kakao.com>
2022-09-22 00:02:56 +0900

    Presentations - CommonPresentations, GameScene 추가

---
+++ Presentations/GameScene/GameScene/GameScene.h
@@ -0,0 +1,18 @@
+//
+//  GameScene.h
+//  GameScene
+//
+//  Created by sudo.park on 2022/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GameScene.
+FOUNDATION_EXPORT double GameSceneVersionNumber;
+
+//! Project version string for GameScene.
+FOUNDATION_EXPORT const unsigned char GameSceneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameScene/PublicHeader.h>
+
+
