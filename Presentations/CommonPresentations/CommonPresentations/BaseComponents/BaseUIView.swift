1892ef5e7524fbb178d44a925309b6e2c5978002
sudo.park <sudo.park@kakao.com>
2022-09-22 00:21:12 +0900

    [#13] BaseUIView, BaseUIViewController 추가

---
+++ Presentations/CommonPresentations/CommonPresentations/BaseComponents/BaseUIView.swift
@@ -0,0 +1,14 @@
+//
+//  BaseUIView.swift
+//  CommonPresentations
+//
+//  Created by sudo.park on 2022/09/22.
+//
+
+import UIKit
+
+
+open class BaseUIView: UIView {
+    
+    
+}
---
+++ Presentations/CommonPresentations/CommonPresentations/BaseComponents/BaseUIViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BaseUIViewController.swift
+//  CommonPresentations
+//
+//  Created by sudo.park on 2022/09/22.
+//
+
+import UIKit
+
+
+open class BaseUIViewController: UIViewController {
+    
+    
+}
