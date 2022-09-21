0504ac5199f1115809c158e1bee1339c2a2432f6
sudo.park <sudo.park@kakao.com>
2022-09-22 00:02:56 +0900

    Presentations - CommonPresentations, GameScene 추가

---
+++ Presentations/CommonPresentations/CommonPresentations/CommonPresentations.h
@@ -0,0 +1,18 @@
+//
+//  CommonPresentations.h
+//  CommonPresentations
+//
+//  Created by sudo.park on 2022/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonPresentations.
+FOUNDATION_EXPORT double CommonPresentationsVersionNumber;
+
+//! Project version string for CommonPresentations.
+FOUNDATION_EXPORT const unsigned char CommonPresentationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonPresentations/PublicHeader.h>
+
+
