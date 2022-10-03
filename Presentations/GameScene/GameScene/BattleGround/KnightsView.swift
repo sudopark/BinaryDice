e9bec35923f4157b993d276552ba5cf4e23f5272
sudo.park <sudo.park@kakao.com>
2022-10-03 21:15:50 +0900

    [#13] KnightsView 여러 플레이어 말 그리는 경우 로직 추가

--- Presentations/GameScene/GameScene/BattleGround/KnightsView.swift
+++ Presentations/GameScene/GameScene/BattleGround/KnightsView.swift
@@ -189,6 +189,175 @@ extension KnightsView {
     }
 }
 
+
+// MARK: - draw knights at upper side
+
+extension KnightsView {
+    
+    func drawKnightsAtUpperSide(_ knights: Knights, at nodeView: NodeView) {
+        self.knights = knights
+        self.internalImageView.clear()
+        switch knights.count {
+        case 1: self.drawKnightAtUpperSide(knights[0], at: nodeView)
+        case 2: self.drawDoubleKnightsAtUpperSide(knights, at: nodeView)
+        case 3: self.drawTrippleKnightsAtUpperSide(knights, at: nodeView)
+        case 4: self.drawAllKnightsAtUpperSide(knights, at: nodeView)
+        default: return
+        }
+    }
+    
+    private func drawKnightAtUpperSide(_ knight: Knight, at nodeView: NodeView) {
+        let (radius, nodeRadius) = (nodeView.knightCircleRadius, nodeView.frame.width)
+        let center = nodeView.center.moved(
+            dx: nodeRadius * cos(CGFloat.pi * 45/180),
+            dy: -nodeRadius * sin(CGFloat.pi * 45/180)
+        )
+        let circle = self.makeKnightLayerCircle(knight, at: center, with: radius)
+        self.internalImageView.drawLayers([circle])
+    }
+    
+    private func drawDoubleKnightsAtUpperSide(
+        _ knights: Knights,
+        at nodeView: NodeView
+    ) {
+        let (radius, nodeCenter, nodeRadius) = (nodeView.knightCircleRadius, nodeView.center, nodeView.frame.width)
+        let (c45, s45) = (cos(CGFloat.pi * 45/180), sin(CGFloat.pi * 45/180))
+        let refPoint = nodeCenter.moved(dx: nodeRadius * c45, dy: -nodeRadius * s45 )
+        let centers: [CGPoint] = [
+            refPoint.moved(dx: radius * c45, dy: radius * s45),
+            refPoint.moved(dx: -radius * c45, dy: -radius * s45)
+        ]
+        let circles = centers.enumerated().map { (offset, center) -> CAShapeLayer in
+            return self.makeKnightLayerCircle(knights[offset], at: center, with: radius)
+        }
+        self.internalImageView.drawLayers(circles)
+    }
+    
+    private func drawTrippleKnightsAtUpperSide(
+        _ knights: Knights,
+        at nodeView: NodeView
+    ) {
+        let (radius, nodeCenter, nodeRadius) = (nodeView.knightCircleRadius, nodeView.center, nodeView.frame.width)
+        let refPoint = nodeCenter.moved(
+            dx: nodeRadius * cos(CGFloat.pi * 45/180),
+            dy: -nodeRadius * sin(CGFloat.pi * 45/180)
+        )
+        let centers: [CGPoint] = [
+            refPoint,
+            refPoint.moved(dy: radius * 2),
+            refPoint.moved(dx: -radius * 2)
+        ]
+        let circles = centers.enumerated().map { (offset, center) -> CAShapeLayer in
+            return self.makeKnightLayerCircle(knights[offset], at: center, with: radius)
+        }
+        self.internalImageView.drawLayers(circles)
+    }
+    
+    private func drawAllKnightsAtUpperSide(
+        _ knights: Knights,
+        at nodeView: NodeView
+    ) {
+        let (radius, nodeCenter, nodeRadius) = (nodeView.knightCircleRadius, nodeView.center, nodeView.frame.width)
+        let refPoint = nodeCenter.moved(
+            dx: nodeRadius * cos(CGFloat.pi * 45/180),
+            dy: -nodeRadius * sin(CGFloat.pi * 45/180)
+        )
+        let centers: [CGPoint] = [
+            refPoint,
+            refPoint.moved(dy: radius * 2),
+            refPoint.moved(dx: -radius * 2),
+            refPoint.moved(dx: -radius * 2, dy: radius * 2)
+        ]
+        let circles = centers.enumerated().map { (offset, center) -> CAShapeLayer in
+            return self.makeKnightLayerCircle(knights[offset], at: center, with: radius)
+        }
+        self.internalImageView.drawLayers(circles)
+    }
+}
+
+
+// MARK: - draw knights at lower side
+
+extension KnightsView {
+
+    func drawKnightsAtLowerSide(_ knights: Knights, at nodeView: NodeView) {
+        self.knights = knights
+        self.internalImageView.clear()
+        switch knights.count {
+        case 1: self.drawKnightAtLowerSide(knights[0], at: nodeView)
+        case 2: self.drawDoubleKnightsAtLowerSide(knights, at: nodeView)
+        case 3: self.drawTrippleKnightsAtLowerSide(knights, at: nodeView)
+        case 4: self.drawAllKnightsAtLowerSide(knights, at: nodeView)
+        default: return
+        }
+    }
+    
+    private func drawKnightAtLowerSide(_ knight: Knight, at nodeView: NodeView) {
+        let (radius, nodeRadius) = (nodeView.knightCircleRadius, nodeView.frame.width)
+        let center = nodeView.center.moved(
+            dx: -nodeRadius * cos(CGFloat.pi * 45/180),
+            dy: nodeRadius * sin(CGFloat.pi * 45/180)
+        )
+        let circle = self.makeKnightLayerCircle(knight, at: center, with: radius)
+        self.internalImageView.drawLayers([circle])
+    }
+    
+    private func drawDoubleKnightsAtLowerSide(_ knights: Knights, at nodeView: NodeView) {
+        let (radius, nodeCenter, nodeRadius) = (nodeView.knightCircleRadius, nodeView.center, nodeView.frame.width)
+        let (c45, s45) = (cos(CGFloat.pi * 45/180), sin(CGFloat.pi * 45/180))
+        let refPoint = nodeCenter.moved(
+            dx: -nodeRadius * cos(CGFloat.pi * 45/180),
+            dy: nodeRadius * sin(CGFloat.pi * 45/180)
+        )
+        let centers: [CGPoint] = [
+            refPoint.moved(dx: -radius * c45, dy: -radius * s45),
+            refPoint.moved(dx: radius * c45, dy: radius * s45)
+        ]
+        let circles = centers.enumerated().map { (offset, center) -> CAShapeLayer in
+            return self.makeKnightLayerCircle(knights[offset], at: center, with: radius)
+        }
+        self.internalImageView.drawLayers(circles)
+    }
+    
+    private func  drawTrippleKnightsAtLowerSide(_ knights: Knights, at nodeView: NodeView) {
+        let (radius, nodeCenter, nodeRadius) = (nodeView.knightCircleRadius, nodeView.center, nodeView.frame.width)
+        let refPoint = nodeCenter.moved(
+            dx: -nodeRadius * cos(CGFloat.pi * 45/180),
+            dy: nodeRadius * sin(CGFloat.pi * 45/180)
+        )
+        let centers: [CGPoint] = [
+            refPoint,
+            refPoint.moved(dy: -radius * 2),
+            refPoint.moved(dx: radius * 2)
+        ]
+        let circles = centers.enumerated().map { (offset, center) -> CAShapeLayer in
+            return self.makeKnightLayerCircle(knights[offset], at: center, with: radius)
+        }
+        self.internalImageView.drawLayers(circles)
+    }
+    
+    private func drawAllKnightsAtLowerSide(
+        _ knights: Knights,
+        at nodeView: NodeView
+    ) {
+        let (radius, nodeCenter, nodeRadius) = (nodeView.knightCircleRadius, nodeView.center, nodeView.frame.width)
+        let refPoint = nodeCenter.moved(
+            dx: -nodeRadius * cos(CGFloat.pi * 45/180),
+            dy: nodeRadius * sin(CGFloat.pi * 45/180)
+        )
+        let centers: [CGPoint] = [
+            refPoint,
+            refPoint.moved(dy: -radius * 2),
+            refPoint.moved(dx: radius * 2),
+            refPoint.moved(dx: radius * 2, dy: -radius * 2)
+        ]
+        let circles = centers.enumerated().map { (offset, center) -> CAShapeLayer in
+            return self.makeKnightLayerCircle(knights[offset], at: center, with: radius)
+        }
+        self.internalImageView.drawLayers(circles)
+    }
+}
+
 private extension CAShapeLayer {
     
     static func circle(
